18bef710cc6f0941889e50635c9d87810fa0ccab
nuclear7777 <spiritvoideu@gmail.com>
2023-10-28 22:31:23 +0300

    django project

---
+++ app/test_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='app-home'),
+    path('about/', views.about, name='about-club'),
+    path('negr/', views.negr, name='negr'),
+]
---
+++ app/test_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('<h1>Главная</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>2 страница</h1>')
+
+
+def negr(request):
+    return HttpResponse('<h1>3 страница</h1>')
+
+
