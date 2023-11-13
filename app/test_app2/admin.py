d26b55bd1a5edaed0f648f2e36228530e820154c
nuclear7777 <spiritvoideu@gmail.com>
2023-11-13 21:20:03 +0300

    admin panel

--- app/test_app2/admin.py
+++ app/test_app2/admin.py
@@ -4,9 +4,15 @@ from .models import Image
 
 admin.site.register(Item)
 
-admin.site.register(Image)
 
 
+
+class CustomAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image', 'price')
+    search_fields = ('title',)
+    list_display_links = ('image',)
+
+admin.site.register(Image, CustomAdmin)
 
 
 
